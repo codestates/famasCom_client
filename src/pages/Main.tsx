5ecec3a117963bc48db30a50d5e7fe136b003787
hanliang38 <kk5448599@gmail.com>
2020-12-01 14:51:54 +0900

    feat/add skeleton pages

---
+++ src/pages/Main.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class Main extends React.Component {
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
+
+export default Main;
