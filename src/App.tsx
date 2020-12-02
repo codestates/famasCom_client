29eae7062b39768f8913a21f216d065e8c75aa05
hanliang38 <kk5448599@gmail.com>
2020-12-02 22:15:08 +0900

    feat/start main & router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import * as React from 'react';
 import { Redirect } from "react-router-dom";
 import './App.css';
-// import Nav from './component/Nav';
-// import { MyPage, Main, Login, SignUp, CouponSetUp, CouponChoose } from './pages';
+import Nav from './components/Nav';
+import { Main, Login, Signup, ComExplain, Community, MobExplain, ModifyInfo, IDK } from './pages/Index';
 
 import {
   BrowserRouter as Router,
@@ -10,118 +10,17 @@ import {
   Route,
 } from "react-router-dom";
 
-// function App() {
-// class App extends React.Component {
-//   constructor(props) {
-//     super(props);
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Router>
+        <Route exact path="/" component={Main} />
+        <Route exact path="/ComExplain" component={ComExplain} />
+        <Route exact path="/MobExplain" component={MobExplain} />
+        <Route exact path="/IDK" component={IDK} />
+      </Router>
+    </div>
+  );
+}
 
-//     this.state = {
-//       user_name: '',
-//       redirect: null
-//     };
-
-//     this.updateUserName = this.updateUserName.bind(this);
-//     this.updateRedirect = this.updateRedirect.bind(this);
-//     this.goToHome = this.goToHome.bind(this);
-//     this.requireAuth = this.requireAuth.bind(this);
-//   }
-
-
-//   componentDidMount() {
-//     const usrName = sessionStorage.getItem('user_name') ? sessionStorage.getItem('user_name') : ''
-//     this.setState({ user_name: usrName })
-//   }
-
-//   updateUserName(name) {
-//     this.setState({ user_name: name });
-//     return;
-//   }
-
-//   updateRedirect(url) {
-//     this.setState({ redirect: url })
-//   }
-
-//   goToHome() {
-//     <Redirect to={this.state.redirect} />
-//     return;
-//   }
-
-//   requireAuth() {
-//     if (sessionStorage.getItem('user_id')) {
-//       return true
-//     } else {
-//       return false
-//     }
-//   }
-
-//   render() {
-
-//     console.log(this.state)
-//     return (
-//       < div className="App" >
-//         <Router >
-//           <Nav
-//             usrUpdate={this.updateUserName}
-//             usrName={this.state.user_name}
-//             updateRedirect={this.updateRedirect}
-//             goToHome={this.goToHome}
-//           />
-//           <Switch>
-//             <Route exact path="/">
-//               <Main />
-//             </Route>
-//             <Route path="/mypage" render={() => {
-//               return this.requireAuth() ? (
-//                 <MyPage />
-//               ) : (
-//                   <Redirect to="/login" />
-//                 )
-//             }} />
-//             <Route path="/login">
-//               <Login usrUpdate={this.updateUserName} usrName={this.state.user_name} />
-//             </Route>
-//             <Route path="/signup">
-//               <SignUp usrUpdate={this.updateUserName} />
-//             </Route>
-
-//             <Route exact path="/couponsetting" render={() => {
-//               return this.requireAuth() ? (
-//                 <CouponChoose />
-//               ) : (
-//                   <Redirect to="/login" />
-//                 )
-//             }} />
-
-//             <Route exact path="/couponsetting/ticket" render={() => {
-//               return this.requireAuth() ? (
-//                 <CouponSetUp />
-//               ) : (
-//                   <Redirect to="/login" />
-//                 )
-//             }} />
-
-//             <Route exact path="/couponsetting/stampboard" render={() => {
-//               return this.requireAuth() ? (
-//                 <StampBoard />
-//               ) : (
-//                   <Redirect to="/login" />
-//                 )
-//             }} />
-//           </Switch>
-//         </Router>
-//       </div >
-//     )
-//   };
-// }
-
-// {/* 
-//   <Route exact path="/home" render={() => (
-//   isLoggedIn() ? (
-//     <Redirect to="/front"/>
-//   ) : (
-//     <Home />
-//   )
-// )}/> 
-// */}
-
-// export default App;
+export default App;
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+
 class Main extends React.Component {
     render() {
         return (
-            <div></div>
-        )
+            <div>
+                <h1>홈</h1>
+                <p>이곳은 홈이에요. 가장 먼저 보여지는 페이지죠.</p>
+            </div>
+        );
     }
 }
 
