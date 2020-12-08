7bbf30f55497faa77db1706551cfed228b524385
hanliang38 <kk5448599@gmail.com>
2020-12-08 13:20:41 +0900

    fix/BtnSection faild

--- src/components/BTNSection.js
+++ src/components/BTNSection.js
@@ -12,23 +12,23 @@ export default function BTNSection() {
             <IconBtn>
                 <Icon>
                     <img className="man-icon" src={man} height="170px" width="150px" alt="man"></img>
-                    <a>60대 김ㅇㅇ씨</a>
                 </Icon>
-                <Link className="explain-list-link" to="/ComExplain"><Button>컴퓨터 사용이 어려워요!</Button></Link>
+                    <span>60대 김ㅇㅇ씨</span>
+                <Link className="explain-list-link" to="/ComExplain"><Button change>컴퓨터 사용이 어려워요!</Button></Link>
             </IconBtn>
             <IconBtn>
                 <Icon>
                     <img className="woman-icon" src={woman} height="160px" width="143px" alt="woman"></img>
-                    <a>50대 박ㅇㅇ씨</a>
-                    </Icon>
-                <Link className="explain-list-link" to="/MobExplain"><Button>스마트폰 사용이 어려워요!</Button></Link>
+                </Icon>
+                    <span>50대 박ㅇㅇ씨</span>
+                <Link className="explain-list-link" to="/MobExplain"><Button change>스마트폰 사용이 어려워요!</Button></Link>
             </IconBtn>
             <IconBtn>
                 <Icon>
                     <img className="oldMan-icon" src={oldMan} height="170px" width="150px" alt="old-man"></img>
-                    <a>70대 이ㅇㅇ씨</a>
                 </Icon>
-                <Link className="explain-list-link" to="/IDK"><Button>하나도 모르겠어요!</Button></Link>
+                    <span>70대 이ㅇㅇ씨</span>
+                <Link className="explain-list-link" to="/IDK"><Button change>하나도 모르겠어요!</Button></Link>
             </IconBtn>
         </Btns>
     </BtnSection>
@@ -36,30 +36,37 @@ export default function BTNSection() {
 }
 
 const BtnSection = styled.div`
-display: flex;
-justify-content: space-around;
-`
+    display: flex;
+    justify-content: center;
+`;
 const Btns = styled.ul`
-
-`
+    list-style:none;
+`;
 const IconBtn = styled.li`
-
-`
-
+    list-style:none;
+`;
 const Icon = styled.div`
-
-`
+    list-style:none;
+`;
 
 const Button = styled.button`
-border-radius: 50px;
-padding: 10px;
-min-width: 120px;
-color: white;
-font-weight: 600px;
--webkit-appearance: none;
-cursor: pointer;
-&:active,
-&:focus {
-    outline: none;
-}
-`
+    height: 170px;
+    width: 600px;
+    font-size: 50px;
+    font-weight: 600px;
+    padding: 10px;
+    min-width: 120px;
+    border-radius: 50px;
+    -webkit-appearance: none;
+    cursor: pointer;
+    &:active,
+    &:focus {
+        outline: none;
+    }
+    background-color: #ecf0f1" 
+    &:hover {
+        .button {
+            background-color: "#7f8c8d"
+        }
+    }
+`;
