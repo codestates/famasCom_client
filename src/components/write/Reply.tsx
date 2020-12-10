f1caf526eb5518d80e0f23f01b60a1cc09e6b4e9
YUBYEONGGUK <godkor200@gmail.com>
2020-12-10 21:31:47 +0900

    it's Implementing

--- src/components/write/Reply.tsx
+++ src/components/write/Reply.tsx
@@ -1,123 +1,135 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
+import { CommentSwappar, Line } from "./style/ReplyStyle"
+import axios from 'axios';
 import moment from 'moment';
 import 'antd/dist/antd.css';
 import Avatar from '@material-ui/core/Avatar';
-import { Comment,Tooltip, Button, Form, List, Input  } from 'antd'
+import { Comment, Tooltip, Button, Form, List, Input } from 'antd'
 import DeleteModal from './DeleteModal'
 import EditModal from './EditModal'
-
-const Replying = styled.div`
-.reply {
-  text-decoration: none;
-  cursor: pointer;
-}
-`;
-
-const Footer = styled.div`
-display: flex;
-`;
-
-const Line = styled.div`
-dispaly: block;
-width: 1022px;
-height: 10px;
-color: #fff;
-`;
+import { type } from 'os';
+import { StyleButton } from "./style/StoryFormstyle.js"
+import AddLike from './AddLike';
 
 type StoryFormProps = {
   datas: any;
   editState: boolean;
   rechatValue: string;
+  commentValue: string;
   deleteModalState: boolean;
   onDeletelick: () => void;
   onEditclick: () => void;
+  handleLike: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  onHandleChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
   handleMsgDelete: (e: React.MouseEvent<HTMLButtonElement>) => void;
   handleRechatChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   handleUdateClick: (e: React.FormEvent<HTMLInputElement>) => void;
   handleReChatClick: (e: React.FormEvent<HTMLInputElement>) => void;
+  onsubmit: (e: any) => void;
 };
 
-const { TextArea } = Input;
+const { TextArea } = Input
+
+// type TextArea = { // The common Part
+//   className?: string;
+//   placeholder?: string;
+// } & ({ // The discriminated union
+//   type?: "text";
+//   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+// } | {
+//   type: "textarea";
+//   onChange?: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
+// })
 
 const Reply = ({ datas,
   deleteModalState, onDeletelick, handleMsgDelete,
-  handleUdateClick, editState, onEditclick}: StoryFormProps) => {
-  
-  console.log("datas!!!!!!!!!!!", datas)
+  handleUdateClick, editState, onEditclick, handleLike, onHandleChange, commentValue, onsubmit }: StoryFormProps) => {
 
-  const [replyState, setReplyState] = useState<boolean>(false);
-  const handleReplyOpen =  (e: React.MouseEvent<HTMLInputElement>) => {
-    e.preventDefault();
-    setReplyState(!replyState);
-    }
-  
-    return (
-      datas === null ? '' : datas.map((item:any) => {
-        console.log(item)
-        const actions:any = [
-          <span className="iLike" style={{ cursor: 'pointer' }}> 좋아요 ♥︎ {item.like}</span>,
-          <span onClick={handleReplyOpen} key="comment-basic-reply-to">댓글 등록</span>,
-          <span className="edit_btn" onClick={onEditclick} style={{ cursor: 'pointer' }}>수정</span>,
-          <span className="delete_btn" key={item.msgId} onClick={onDeletelick} style={{ cursor: 'pointer' }}>삭제</span>
-        ];
-        return (
-          <React.Fragment>
-            <Comment
-              key={item.msgId}
-              // autor={item.userName}
-              actions={actions}
-              avatar={
-                <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
-              }
-              content={
-                <iframe className="story" frameBorder="0" width={100} srcDoc={item.msg}></iframe>
-              }
-              datetime={
-                <Tooltip title={moment().subtract(1, 'days').format('YYYY-MM-DD HH:mm:ss')}>
-                  <span>{moment().subtract(1, 'days').fromNow()}</span>
-                </Tooltip>
-              } >
-              {/* {item.comments.length === 0 ? <></> :
-                item.comments.map((cmt:any) => {
-                  console.log("cmt>>>>>>>>>>>>>>>>>>>🐶", cmt)
-                  return (
-                    <Comment
-                    key = {cmt.index}
-                    actions = {actions}
-                    avatar={<Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />}
-                    author = {cmt[0]}
-                    content = {cmt[1]}
-                    datetime={
-                  <Tooltip title={moment().format('YYYY-MM-DD HH:mm:ss')}>
-                    <span>{moment().fromNow()}</span>
-                  </Tooltip>
-                }  />
-                  )
-                })
-              } */}
-              <DeleteModal onDeletelick={onDeletelick} deleteModalState={deleteModalState} handleMsgDelete={handleMsgDelete} msgid={item.msgId} />
-              <EditModal msgid={item.msgId} onEditclick={onEditclick} editState={editState} handleUdateClick={handleUdateClick} />
-              <Footer>
-                {replyState &&
-                  <>
-                    <Form.Item>
-                      <TextArea rows={4} /*onChange={onHandleChange} value={CommentValue} */ />
-                    </Form.Item>
-                    <Form.Item>
-                      <Button /*htmlType="submit" loading={submitting} /*onClick={onsubmit}*/ type="primary">
-                        댓글 등록
+  //console.log("🚀 ~ file: Reply.tsx ~ line 44 ~ datas", datas)
+  //대댓글 랜더 관련 훅 
+  const [commentOpen, setCommentOpen] = useState<boolean>(false);
+  //대댓글 랜더 관련 토글
+  const handleCommentOpen = () => {
+    setCommentOpen(!commentOpen);
+  }
+
+  //대댓글 관련 훅
+  const [replyOpen, setReplyOpen] = useState<boolean>(false);
+
+  //대댓글 관련 토글 
+  const handleReplyOpen = () => {
+    setReplyOpen(!replyOpen);
+  }
+  //좋아요 관련 
+
+  //comment Actions
+  const actions: any = [
+    <span onClick={handleLike} className="iLike" style={{ cursor: 'pointer' }}> 좋아요 ♥︎ {datas.goodLike}</span>,
+    <span onClick={handleReplyOpen} key="comment-basic-reply-to">댓글 등록</span>,
+    <span onClick={onEditclick} className="edit_btn" style={{ cursor: 'pointer' }}>수정</span>,
+    <span onClick={onDeletelick} className="delete_btn" style={{ cursor: 'pointer' }}>삭제</span>,
+    <div onClick={handleCommentOpen} className="comment_btn" style={{ cursor: 'pointer' }}>댓글({datas.comments.length})</div>
+  ]
+
+  return (
+    <CommentSwappar>
+      <Comment
+        actions={actions}
+        key={datas.msgId}
+        author={datas.userName}
+        avatar={
+          <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
+        }
+        content={datas.msg}
+        datetime={
+          <Tooltip title={moment().subtract(1, 'days').format('YYYY-MM-DD HH:mm:ss')}>
+            <span>{moment().subtract(1, 'days').fromNow()}</span>
+          </Tooltip>
+        } >
+        {commentOpen &&
+          <List>
+            {datas.comments.map((cmt: any) => (<Comment avatar={
+              <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
+            } author={cmt[0]} content={cmt[1]} />
+            )
+            )
+            }</List>}
+        <AddLike handleLike={handleLike} msgid={datas.msgId} />
+        <DeleteModal onDeletelick={onDeletelick} deleteModalState={deleteModalState} handleMsgDelete={handleMsgDelete} msgid={datas.msgId} />
+        <EditModal onEditclick={onEditclick} editState={editState} handleUdateClick={handleUdateClick} msgid={datas.msgId} />
+      </Comment>
+
+      {replyOpen &&
+        <Form>
+          <Form.Item>
+            <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
+            <TextArea rows={1} onChange={onHandleChange} value={commentValue} />
+            <Button htmlType="submit" /*loading={submitting}*/ type="primary" onClick={onsubmit} id={datas.msgId}>
+              댓글 등록
             </Button>
-                    </Form.Item>
-                  </>}
-        
-              </Footer>
-            
-            </Comment>
-            <Line><br></br></Line>
-          </React.Fragment>
+          </Form.Item>
+        </Form>
+      }
+
+    </CommentSwappar>
+  )
+}
+{/* {item.comments.length === 0 ? <></> :
+      item.comments.map((cmt:any) => {
+        console.log("cmt>>>>>>>>>>>>>>>>>>>🐶", cmt)
+        return (
+          <Comment
+          key = {cmt.index}
+          actions = {actions}
+          avatar={<Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />}
+          author = {cmt[0]}
+          content = {cmt[1]}
+          datetime={
+        <Tooltip title={moment().format('YYYY-MM-DD HH:mm:ss')}>
+          <span>{moment().fromNow()}</span>
+        </Tooltip>
+      }  />
         )
       })
-    )
-  }
-  export default Reply
+    } */}
+export default Reply
--- src/components/write/StoryForm.tsx
+++ src/components/write/StoryForm.tsx
@@ -1,94 +1,72 @@
 import React from 'react';
-import styled from 'styled-components';
-import Responsive from '../common/Responsive';
 import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css'; 
+import 'react-quill/dist/quill.snow.css';
 import Reply from './Reply'
+import Responsive from '../common/Responsive';
+import styled from 'styled-components';
+const EditorBlock = styled(Responsive)`padding: 10px;`;
+import { EntiryWrapper, QuillWrapper, StyleButton, Line } from "./style/StoryFormstyle.js"
+import { Comment } from 'antd';
+
 // 댓글 폼을 나타내는 컴포넌트
+
 type StoryFormProps = {
   datas: any;
   editorHtml: string;
   editState: boolean;
   rechatValue: string;
+  commentValue: string;
   deleteModalState: boolean;
   onDeletelick: () => void;
   onEditclick: () => void;
-  handleMsgDelete: (e: React.MouseEvent<HTMLButtonElement>)=> void;
-  handleRechatChange: (e: React.ChangeEvent<HTMLInputElement>)=> void;
-  handleUdateClick: (e: React.FormEvent<HTMLInputElement>)=> void;
+  onHandleChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  handleLike: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  handleMsgDelete: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  handleRechatChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  handleUdateClick: (e: React.FormEvent<HTMLInputElement>) => void;
   handleStoryChange: (html: any) => void;
-  handleSubmitClick: (e: React.MouseEvent<HTMLButtonElement>)=> void;
-  handleReChatClick: (e: React.FormEvent<HTMLInputElement>)=> void;
+  handleSubmitClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  onsubmit: (e: any) => void;
+  handleReChatClick: (e: React.FormEvent<HTMLInputElement>) => void;
 };
 
-const StoryForm = ({rechatValue, editState, deleteModalState, onDeletelick, onEditclick, handleMsgDelete, handleRechatChange,
-  handleUdateClick, datas, editorHtml, handleStoryChange, handleSubmitClick, handleReChatClick}: StoryFormProps) => {
+const StoryForm = ({ rechatValue, editState, deleteModalState, onDeletelick, onEditclick, handleMsgDelete, handleRechatChange,
+  handleUdateClick, datas, editorHtml, handleStoryChange, handleSubmitClick, handleReChatClick, handleLike, onHandleChange, commentValue, onsubmit }: StoryFormProps) => {
 
-  console.log("🚀 ~ file: StoryForm.js ~ line 12 ~ StoryForm ~ datas", datas)
-  
   return (
-    <React.Fragment>
+    <EntiryWrapper>
       <EditorBlock>
         <QuillWrapper>
           <ReactQuill theme="snow" value={editorHtml} onChange={handleStoryChange} />
         </QuillWrapper>
         <StyleButton onClick={handleSubmitClick}>등록</StyleButton>
       </EditorBlock>
+
       <Line><br></br></Line>
-      <Reply datas={datas}
-        handleMsgDelete={handleMsgDelete} deleteModalState={deleteModalState} onDeletelick={onDeletelick}
-        editState={editState} handleUdateClick={handleUdateClick} onEditclick={onEditclick} rechatValue={rechatValue}
-        handleRechatChange={handleRechatChange} handleReChatClick={handleReChatClick}/>
-    </React.Fragment>
-    )
+
+
+      <p>댓글</p>{/* 메세지를 하나씩!! 뿌려줌 */}
+      {!datas ? '' : datas.map((item: any) => (
+        <>
+          <Reply datas={item}
+            onsubmit={onsubmit}
+            onHandleChange={onHandleChange}
+            handleLike={handleLike}
+            handleMsgDelete={handleMsgDelete}
+            deleteModalState={deleteModalState}
+            onDeletelick={onDeletelick}
+            editState={editState}
+            handleUdateClick={handleUdateClick}
+            onEditclick={onEditclick}
+            commentValue={commentValue}
+            rechatValue={rechatValue}
+            handleRechatChange={handleRechatChange}
+            handleReChatClick={handleReChatClick} />
+        </>)
+      )}
+    </EntiryWrapper>
+  )
 }
+
 
 export default StoryForm;
-
-const EditorBlock = styled(Responsive)`
-padding-top: 1rem;
-`;
-
-const QuillWrapper = styled.div`
-
-.ql-container {
-  min-height: 50px;
-  border-radius: 10px;
-  padding: 0;
-  font-size: 1.125rem;
-  line-height: 1.5;
-}
-.ql-container .ql-blank ::before {
-  left 0px
-}
-`;
-const StyleButton = styled.button`
-border: none;
-cursor: pointer;
-outline: none;
-margin: 0.6rem 0 0;
-padding-top: 0.75rem;
-padding-bottom: 0.75rem;
-width: 8rem;
-height: 2.75rem;
-border-radius: 4px;
-font-size: 1.125rem;
-font-weight: bold;
-font-family: '맑은 고딕'
-color: black;
-background: #6EB584;
-display: inline-block;
-float: right;
-text-align: center;
-line-height: 22px;
-&:hover {
-  color:blue;
-  border: 2px solid blue;
-}
-`;
-
-const Line = styled.div`
-dispaly: block;
-width: 1022px;
-height: 10px;
-`;
