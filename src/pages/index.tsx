8606a110301af08b0a2bf59600b5be24a412f97f
IMMINJU <minju2996@gmail.com>
2020-12-16 05:33:52 +0900

    [client]feat/ add letcture nav & route, style/ modify letcture page & page banners, resource/ add img files & lecture texts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
-export { default as ComExplain } from './ComExplain';
+export { Computer, Mobile, IDK } from './letctures';
 export { default as Signup } from './Signup';
+export { default as Main } from './Main';
 export { default as Login } from './Login';
 export { default as ModifyInfo } from './ModifyInfo';
 export { default as Community } from './Community';
-export { default as IDK } from './IDK';
-export { default as MobExplain } from './MobExplain';
 export { default as NotFound } from './NotFound';
-export { default as Main } from './Main';
-
-//
+export { default as Lecture } from './Lecture';
+export { default as Game } from './Game';
---
+++ src/textBody/lectureText/index.tsx
@@ -0,0 +1,7 @@
+export { default as ComBody1 } from './ComBody1';
+export { default as ComBody2 } from './ComBody2';
+export { default as ComBody3 } from './ComBody3';
+export { default as MobBody1 } from './MobBody1';
+export { default as MobBody2 } from './MobBody2';
+export { default as IDKBody1 } from './IDKBody1';
+export { default as IDKBody2 } from './IDKBody2';
