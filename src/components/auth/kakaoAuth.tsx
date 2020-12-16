45a7fd3099f070e9e7a82f6c4515d28be8ef2a1d
YUBYEONGGUK <godkor200@gmail.com>
2020-12-16 19:05:53 +0900

    solved Callstack

--- src/components/auth/kakaoAuth.tsx
+++ src/components/auth/kakaoAuth.tsx
@@ -1,39 +1,43 @@
+import * as dotenv from 'dotenv';
 import KakaoLogin from 'react-kakao-login'
 import React from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
 import { useHistory } from "react-router-dom";
+dotenv.config();
 
-const key = process.env.kakaoKey
 
-
+const key = process.env.KAKAO_KEY
 export default {
     title: "KakaoLogin",
     component: KakaoLogin,
 };
+const _method = {
+    async _signIn(result: any) {
+        return await axios.post(`https://jven72vca8.execute-api.ap-northeast-2.amazonaws.com/dev/kakaoSignIn/${result.profile.id}`)
+    },
+    async _signUp(result: any) {
+        return await axios.post("https://jven72vca8.execute-api.ap-northeast-2.amazonaws.com/dev/kakaoSignUp", {
+            userId: String(result.profile.id),
+            email: result.profile.kakao_account.email,
+            nickName: result.profile.kakao_account.profile.nickname,
+            profileImage: result.profile.kakao_account.profile.thumbnail_image_url
+        })
+    }
+}
 
 
 export const Kakao = () => {
     const history = useHistory();
     const kakaoLogin = {
-        _success(result: any) {
+        async _success(result: any) {
             if (result) {
-                axios.post("https://jven72vca8.execute-api.ap-northeast-2.amazonaws.com/dev/kakaoSiginIn", {
-                    email: result.profile.kakao_account.email,
-                    nickName: result.profile.kakao_account.profile.nickname,
-                    profileImage: result.profile.kakao_account.profile.thumbnail_image_url
-                }).then((res) => {
+                await _method._signIn(result).then(res => {
                     localStorage.setItem("token", res.data.token)
                     localStorage.setItem("kakao_token", result.response.access_token)
-                }).then(() => {
-                    if (localStorage.getItem("token") && localStorage.getItem("kakao_token")) {
-                        history.push("/");
-                    }
-                }).catch((res) => {
-                    if (res.status === 409) {
-                        alert('로그인에 실패하였습니다');
-                    }
-                });
+                    history.push("/");
+                }).catch(err =>
+                    _method._signUp(result).then(res => this._success(result)))
             }
         }
     }
